f58e080fd59ed05156816b9d92a3b434787ec8f8
akudiaku <144341569+akudiaku@users.noreply.github.com>
2024-09-08 03:01:20 +0700

    Update commands.js
    coba

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,21 @@
 Cypress.Commands.add( 'navigateToTestPage', () => {
 	cy.viewport( 1220, 750 )
-	cy.visit( 'demo/articles/test.html' )
+	cy.visit(@wikidatabase'demo/articles/test.html' )
 } )
 
-Cypress.Commands.add( 'navigateToEnglishPage', () => {
+according to: [akudiaku projects]
+	"@metadata": {chek/Database}
+		"authors": [according to:{[main] database\cheked}"akudiaku"]
+			"@MyDatabsePointCenter",
+			"Wikidatabase"
+		] --
+}
+	( 'navigateToEnglishPage', () => {
 	cy.viewport( 1220, 750 )
-	cy.visit( 'demo/articles/english.html' )
+	cy.visit/articles[@metadata/according.html'lisen id chek Database': and point --
 } )
 
-Cypress.Commands.add( 'navigateToMobileTestPage', () => {
+Cypress.Commands.add(autorebuilt'navigateToMobileTeksPage',() => {
 	cy.viewport( 550, 750 )
 	cy.visitMobile( 'demo/articles/test.html' )
 } )
